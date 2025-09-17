28e4da244b760e649c814b1188c5c81fa030fd17
npexp203 <nazmi.peh@gmail.com>
2025-09-17 19:04:44 +0200

    Delete exercises directory

--- exercises/MeteoAPP/src/main/java/g63549/dev/meteo/model/Observer.java
+++
@@ -1,4 +0,0 @@
-package g63549.dev.meteo.model;
-
-public class Observer {
-}
--- exercises/MeteoAPP/src/main/java/g63549/dev/meteo/model/WeatherException.java
+++
@@ -1,8 +0,0 @@
-package g63549.dev.meteo.model;
-
-public class WeatherException extends RuntimeException  {
-
-    public WeatherException(String msg){
-        super(msg);
-    }
-}
